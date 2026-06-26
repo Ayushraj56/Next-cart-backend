ec4dae74350f6885342530ab65040ec60ef3c454
Ayush Raj <ayushrajsingh9065@gmail.com>
2026-06-26 13:23:36 +0530

    fix: use env vars for Firebase Admin instead of serviceAccountKey.json

--- config/db.js
+++ config/db.js
@@ -1,22 +1,31 @@
-import mongoose from "mongoose";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const uri = process.env.MONGO_URI || "mongodb+srv://ay065:ayushragh9065@ecommerce.g4fvfwx.mongodb.net/?retryWrites=true&w=majority";
+
+let client;
+let db;
 
 const connectDB = async () => {
-
     try {
+        client = new MongoClient(uri, {
+            serverSelectionTimeoutMS: 5000,
+            socketTimeoutMS: 45000,
+        });
 
-        await mongoose.connect(
-            "mongodb://127.0.0.1:27017/ecommerce"
-        );
+        await client.connect();
+
+        db = client.db("ecommerce");
 
         console.log("MongoDB Connected");
 
+        return db;
+    } catch (error) {
+        console.error("MongoDB Connection Error:", error);
+        process.exit(1);
     }
+};
 
-    catch (error) {
-
-        console.log(error);
-
-    }
-}
-
-export default connectDB;
+export default connectDB

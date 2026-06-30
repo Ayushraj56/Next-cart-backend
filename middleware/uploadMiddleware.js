77eba509bebc1c0c0ce1ba1167d9b72e179302eb
Ayush Raj <ayushrajsingh9065@gmail.com>
2026-06-30 12:11:35 +0530

    update upload middleware

--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -1,20 +1,21 @@
 import multer from "multer";
+import fs from "fs";
+import path from "path";
+
+const uploadPath = path.join("public", "uploads");
+
+if (!fs.existsSync(uploadPath)) {
+    fs.mkdirSync(uploadPath, { recursive: true });
+}
 
 const storage = multer.diskStorage({
- destination: (req, file, cb) => {
-      cb(null, "public/uploads");
-   },
+    destination: (req, file, cb) => {
+        cb(null, uploadPath);
+    },
 
-   filename: (req, file, cb) => {
-      cb(
-         null,
-         Date.now() + "-" + file.originalname
-      );
-   }
-})
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}-${file.originalname}`);
+    }
+});
 
-const upload = multer({
-   storage
-})
-
-export default upload;
+export default multer({ storage });
